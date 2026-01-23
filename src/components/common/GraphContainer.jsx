d164d90c742612d20630ff1d60921f0bfcfd2ffe
Desooe-pro <sacha.allardin06@gmail.com>
2026-01-23 13:51:08 +0100

    micro correctif

--- src/components/common/GraphContainer.jsx
+++ src/components/common/GraphContainer.jsx
@@ -64,7 +64,7 @@ function GraphContainer({
                 value: chartConfig[param].label,
                 angle: -90,
                 position: "insideLeft",
-                offset: 20,
+                offset: index % 2 === 1 ? 40 : 20,
                 style: { textAnchor: "middle" },
               }}
             />
